12940bc31050c27f08efcc0edf5d8c432c11b7b7
Rafael Cortês <mrfyda@gmail.com>
2016-10-31 09:16:16 +0000

    add complexity

--- newfile.js
+++ newfile.js
@@ -2,7 +2,19 @@ function a(x) {
     if (true) {
         return x; // 1st path
     } else if (false) {
-        return x+1; // 2nd path
+        if (false) {
+            return x+1; // 2nd path
+        } else {
+            if (false) {
+                return x+1; // 2nd path
+            } else {
+                if (false) {
+                    return x+1; // 2nd path
+                } else {
+                    return 4; // 3rd path
+                }
+            }
+        }
     } else {
         return 4; // 3rd path
     }
