66eeb8389eba43abbbb7aa54daf0082fdbf67db7
Rafael Cortês <mrfyda@gmail.com>
2016-10-31 09:49:00 +0000

    Update newfile.js

--- newfile.js
+++ newfile.js
@@ -19,3 +19,17 @@ function a(x) {
         return 4; // 3rd path
     }
 }
+
+function a(x) {
+  if (true) {
+    return x;
+  } else if (false) {
+    return x + 1;
+  } else if (true) {
+    return 4;
+  } else if (true) {
+    return 5;
+  } else {
+    return 6;
+  }
+}
